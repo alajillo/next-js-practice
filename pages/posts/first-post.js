4323b362bc9dc95bf73c9cc4b439543156e04672
yunsangjun <alajillo2021@gmail.com>
2022-05-21 00:26:45 +0900

    learn css-module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,29 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from "next/script";
+import Layout from '../../components/layout';
+
 export default function FirstPost(){
     return (
-        <>
-            <h1>First Post</h1>
-            <Image
-                src="/images/test.jpeg"
-                height={144}
-                width={144}
-                alt="Your Name"
-            />
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
+
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>First Post</h1>
+                <Image
+                    src="/images/test.jpeg"
+                    height={144}
+                    width={144}
+                    alt="Your Name"
+                />
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
+
     )
 }
